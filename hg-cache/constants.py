54aaa71fccbfa31d1aa1a5b8cd9a0968517ee685
worldemar <woldemar@mimas.ru>
2020-11-10 16:17:20 +0300

    add search for hg executable

--- hg-cache/constants.py
+++ hg-cache/constants.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import subprocess
 
 
 def ENVVAR_HG_CACHE():
@@ -12,8 +13,24 @@ def IS_PYTEST():
     return "pytest" in sys.modules
 
 
-def EXE_HG():
-    return "hg"
+def EXE_HG(sys_executable = sys.executable):
+    hg_names = [os.path.sep + "hg.exe", os.path.sep + "hg"]
+    hg_full_path = ""
+    for hg_name in hg_names:
+        if sys_executable.endswith(hg_name):
+            hg_full_path = os.path.abspath(sys_executable)
+    if hg_full_path:
+        return hg_full_path
+    for hg_name in hg_names:
+        # importing pip is not supported by devs, they recommend this instead
+        cmd = [sys_executable, "-m", "pip", "show", "-f", "mercurial"]
+        mercurial_files = subprocess.check_output(cmd).splitlines()
+        module_location = filter(lambda x: x.startswith("Location: "), mercurial_files)[0].split(": ")[1].strip()
+        hg_paths = filter(lambda x, name=hg_name: x.endswith(name), mercurial_files)
+        if hg_paths:
+            hg_full_path = os.path.abspath(os.path.join(module_location, hg_paths[0].strip()))
+            break
+    return hg_full_path
 
 
 def HG_PLUGIN_ARGS(use_self):
